f43b49070ba5df6c3dd62f7eb15ea4075f3dce62
Daniel Frank <1.daniel.frank@gmail.com>
2020-08-14 23:55:29 +0200

    fixed uart bug (hopefully), increased baudrate to 460800

--- src/uart.c
+++ src/uart.c
@@ -24,8 +24,11 @@ void uart_init()
 	gpio_pinCfg(pinRxD, MODE_AF, 7);
 
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; //enable UART1 clock (84MHz)
-	USART1->BRR = (22<<4) | 13;//Baudrate = 115200, 84MHz / (2 * 8 * 230400) = 22.786, 0.786 * 16 = ~13
-	USART1->CR1 |= (USART_CR1_TCIE | USART_CR1_RXNEIE); //transmission complete & Rx not empty interrupts enable
+//	USART1->BRR = (45<<4) | 9;//Baudrate = 115200, 84MHz / (2 * 8 * 115200) ~ 45.5625, 0.5625 * 16 = 9
+//	USART1->BRR = (22<<4) | 13;//Baudrate = 230400, 84MHz / (2 * 8 * 230400) ~ 22.8125, 0.8125 * 16 = 13
+//	USART1->BRR = (5<<4) | 11;//Baudrate = 921600, 84MHz / (2 * 8 * 921600) ~ 5.6875, 0.6875 * 16 = 11
+	USART1->BRR = (11<<4) | 6;//Baudrate = 460800, 84MHz / (2 * 8 * 460800) ~ 11.375, 0.375 * 16 = 6
+	USART1->CR1 |= USART_CR1_RXNEIE; //rx not empty interrupt enable
 	USART1->CR1 |= (USART_CR1_TE | USART_CR1_RE); //enable Tx & Rx
 	NVIC_EnableIRQ(USART1_IRQn); //enable UART1 global interrupts
 	NVIC_SetPriority(USART1_IRQn, 7); //medium interrupt priority
@@ -35,10 +38,10 @@ void uart_init()
 
 void USART1_IRQHandler(void)
 {
-	if(USART1->SR & USART_SR_TC) //transmission complete (also fires after reset, not a problem)
+	if(USART1->SR & USART_SR_TXE) //tx data register empty
 	{
-		USART1->SR &= ~USART_SR_TC; //clear interrupt flag
-		ringbuffer_pop(&uart_tx_rb, (uint8_t*)(&USART1->DR)); //send data if more data to send available
+		if(ringbuffer_pop(&uart_tx_rb, (uint8_t*)(&USART1->DR)) == RB_ERROR_UNDERFLOW) //send data if more data to send available, clears TXE flag
+			USART1->CR1 &= ~USART_CR1_TXEIE; //no more data, transfer done --> disable TXE interrupt
 	}
 
 	if(USART1->SR & USART_SR_RXNE) //Rx data register not empty
@@ -51,11 +54,10 @@ void USART1_IRQHandler(void)
 
 static void uart_startFifoTransmit()
 {
-	if(USART1->SR & USART_SR_TXE) //if Tx data register is empty TODO: and not busy?
+	if(!(USART1->CR1 & USART_CR1_TXEIE)) //if TXE interrupt disabled --> no transfer in progress
 	{
-		NVIC_DisableIRQ(USART1_IRQn); //disable UART1 global interrupts TODO: is it possible to make this interrupt-safe?
 		ringbuffer_pop(&uart_tx_rb, (uint8_t*)(&USART1->DR)); //send first byte to start transmission TODO: add buffer underflow error
-		NVIC_EnableIRQ(USART1_IRQn); //enable UART1 global interrupts
+		USART1->CR1 |= USART_CR1_TXEIE; //enable TXE interrupt
 	}
 }
 
