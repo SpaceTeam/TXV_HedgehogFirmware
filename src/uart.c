792f16f0f2f568170e2546856f79b474d22c81a4
Franky333 <1.daniel.frank@gmail.com>
2020-12-04 17:40:21 +0100

    lowered baud rate

--- src/uart.c
+++ src/uart.c
@@ -26,8 +26,8 @@ void uart_init()
 
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; //enable UART1 clock (84MHz)
 //	USART1->BRR = (45<<4) | 9;//Baudrate = 115200, 84MHz / (2 * 8 * 115200) ~ 45.5625, 0.5625 * 16 = 9
-//	USART1->BRR = (22<<4) | 13;//Baudrate = 230400, 84MHz / (2 * 8 * 230400) ~ 22.8125, 0.8125 * 16 = 13
-	USART1->BRR = (11<<4) | 6;//Baudrate = 460800, 84MHz / (2 * 8 * 460800) ~ 11.375, 0.375 * 16 = 6
+	USART1->BRR = (22<<4) | 13;//Baudrate = 230400, 84MHz / (2 * 8 * 230400) ~ 22.8125, 0.8125 * 16 = 13
+//	USART1->BRR = (11<<4) | 6;//Baudrate = 460800, 84MHz / (2 * 8 * 460800) ~ 11.375, 0.375 * 16 = 6
 //	USART1->BRR = (5<<4) | 11;//Baudrate = 921600, 84MHz / (2 * 8 * 921600) ~ 5.6875, 0.6875 * 16 = 11
 	USART1->CR1 |= USART_CR1_RXNEIE; //rx not empty interrupt enable
 	USART1->CR1 |= (USART_CR1_TE | USART_CR1_RE); //enable Tx & Rx
